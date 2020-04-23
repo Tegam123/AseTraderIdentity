907142fe4fe6ab85bee7c62b1e566e837f157c45
Lena Todnem <lena.todnem@gmail.com>
2020-04-23 10:40:04 +0200

    New class added

---
+++ AseTrader/Models/AlpacaDependencies/AlpacaClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AseTrader.Models.AlpacaDependencies
+{
+    public class AlpacaClient
+    {
+    }
+}
